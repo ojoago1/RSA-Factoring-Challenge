d78493d20acdfd10f29438c3a621b06b29f26a21
Judith Oguche <judithoguche@gmail.com>
2022-12-25 21:42:13 +0100

    token

--- token_op.c
+++ token_op.c
@@ -1,1 +1,24 @@
-nchjh
+#include "rsa.h"
+
+/**
+*tok_arr - creates array of tokens from given input
+*@lineptr: The line to be tokenized
+*@tokens: Empty array to be fild with tokens
+*Return: Nothing
+*/
+
+void tok_arr(char *lineptr, char *tokens[])
+{
+	ssize_t i;
+	char *token;
+
+	token = strtok(lineptr, " \t\n");
+	i = 0;
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		token = strtok(NULL, " \t\n");
+		i++;
+	}
+	tokens[i] = NULL;
+}
