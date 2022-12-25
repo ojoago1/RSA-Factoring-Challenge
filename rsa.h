261d057a257e01d8de619f33f21e84b6d08062ff
Judith Oguche <judithoguche@gmail.com>
2022-12-25 21:41:15 +0100

    rsa header file

--- rsa.h
+++ rsa.h
@@ -1,1 +1,19 @@
-ghcfjuy
+#ifndef RSA_H
+#define RSA_H
+
+#define  _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+void tok_arr(char *lineptr, char *tokens[]);
+bool check(int factor);
+void factorizer(char *num);
+void file_op(char *filename);
+int second_loop(int i, int n);
+void free_tok(char **tok);
+
+#endif /* RSA_H */
