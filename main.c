ff0cfde88281601f8dd2d66767cbf8b2a45a5e37
Judith Oguche <judithoguche@gmail.com>
2022-12-25 21:39:03 +0100

    main file

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
-kjfhuifh
+#include "rsa.h"
+
+/**
+*main - Entry point
+*@argc: Number of arguments
+*@argv: Array of arguments
+*Return: 0 (success)
+*/
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE factors file\n");
+		exit(EXIT_FAILURE);
+	}
+	file_op(argv[1]);
+	return (0);
+}
